c9d4b426591a99c7a7d8e9d9527d307f75cfd436
Jack <71041042+jackakay@users.noreply.github.com>
2022-11-11 23:14:45 +0000

    basic pathfinding
    Currently doesnt find the shortest path or go around obstacles. It just finds a simple path. Still working on it.

--- Pathfinding/Pathfinding/Form1.Designer.cs
+++ Pathfinding/Pathfinding/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -76,12 +77,25 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Start";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(31, 288);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 53);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Reset";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(959, 539);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -99,5 +113,6 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private Button button5;
     }
 }
--- Pathfinding/Pathfinding/Form1.cs
+++ Pathfinding/Pathfinding/Form1.cs
@@ -11,22 +11,22 @@ namespace Pathfinding
         public static bool Start;
         public static bool End;
         public static bool Obstacle;
-        
+
         public Form1()
         {
             InitializeComponent();
             grid = createGrid();
-            foreach(Panel[] p in grid)
+            foreach (Panel[] p in grid)
             {
                 foreach (Panel p1 in p)
                 {
-                    
+
                     this.Controls.Add(p1);
                 }
             }
-            
+
         }
-       
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -38,61 +38,63 @@ namespace Pathfinding
             Panel[][] grid = new Panel[12][];
 
             Point startLoc = new Point(325, 46);
-            
+
             int change = 39;
-            
+
             for (int x = 0; x < gridSize; x++)
             {
                 grid[x] = new Panel[gridSize];
                 for (int y = 0; y < gridSize; y++)
                 {
-                    
+
                     Panel panel = new Panel
                     {
                         Name = $"{x} , {y}",
                         Size = new Size(33, 33),
                         Location = new Point((startLoc.X + (change * x)), (startLoc.Y + (change * y))),
                         BorderStyle = BorderStyle.FixedSingle,
-                        
+
                     };
                     panel.MouseClick += new MouseEventHandler(pressed);
 
 
 
                     grid[x][y] = panel;
-                    
-                    
+
+
                 }
             }
             return grid;
         }
-         public static void pressed(object sender, MouseEventArgs e)
+        public static void pressed(object sender, MouseEventArgs e)
         {
             Panel? panel = sender as Panel;
             if (Start)
             {
                 panel.BackColor = Color.Green;
-            }else if (End )
+            }
+            else if (End)
             {
-                panel.BackColor= Color.Red;
-            }else if (Obstacle)
+                panel.BackColor = Color.Red;
+            }
+            else if (Obstacle)
             {
                 panel.BackColor = Color.Black;
             }
         }
-       
+
         private void button1_Click(object sender, EventArgs e)
         {
             Start = true;
             End = false;
-            Obstacle= false;
+            Obstacle = false;
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Start=false;
-            End=true;
+            Start = false;
+            End = true;
             Obstacle = false;
         }
 
@@ -101,6 +103,179 @@ namespace Pathfinding
             Start = false;
             End = false;
             Obstacle = true;
+        }
+
+        private Panel[] getAdjacent(Panel[][] grid, Location coords)
+        {
+            Panel[] adjacent = new Panel[8];
+
+            adjacent[0] = grid[coords.X - 1][coords.Y - 1];
+            adjacent[1] = grid[coords.X - 1][coords.Y];
+            adjacent[2] = grid[coords.X - 1][coords.Y + 1];
+            adjacent[3] = grid[coords.X][coords.Y - 1];
+            adjacent[4] = grid[coords.X][coords.Y + 1];
+            adjacent[5] = grid[coords.X + 1][coords.Y - 1];
+            adjacent[6] = grid[coords.X + 1][coords.Y];
+            adjacent[7] = grid[coords.X + 1][coords.Y + 1];
+
+            return adjacent;
+        }
+
+        private Location? getEnd(Panel[][] grid)
+        {
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j].BackColor == Color.Red)
+                    {
+                        return new Location(i, j);
+                    }
+                }
+            }
+            return null;
+        }
+        private Location? getStart(Panel[][] grid)
+        {
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j].BackColor == Color.Green)
+                    {
+                        return new Location(i, j);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Location start = getStart(grid);
+            Location end = getEnd(grid);
+
+            int XDistance = 0;
+            int YDistance = 0;
+            if (start.X > end.X)
+            {
+                XDistance = start.X - end.X;
+                if (start.Y < end.Y)
+                {
+                    YDistance = end.Y - start.Y;
+                    for (int i = XDistance; i > 0; i--)
+                    {
+                        if (grid[start.X - i][start.Y].BackColor == Color.Green || grid[start.X - i][start.Y].BackColor == Color.Red) continue;
+
+
+
+                        grid[start.X - i ][start.Y].BackColor = Color.Blue;
+
+
+
+                    }
+                    for (int i = 0; i < YDistance; i++)
+                    {
+                        if (grid[start.X - XDistance][start.Y + i].BackColor == Color.Green || grid[start.X - XDistance][start.Y + i].BackColor == Color.Red) continue;
+
+
+                        grid[start.X - XDistance][start.Y + i].BackColor = Color.Blue;
+
+                    }
+                }
+                else
+                {
+                    YDistance = start.Y - end.Y;
+                    for (int i = XDistance; i > 0; i--)
+                    {
+                        if (grid[start.X - i][start.Y].BackColor == Color.Green || grid[start.X - i][start.Y].BackColor == Color.Red) continue;
+
+
+
+                        grid[start.X - i][start.Y].BackColor = Color.Blue;
+
+                    }
+                    for (int i = 0; i < YDistance; i++)
+                    {
+                        if (grid[start.X - XDistance][start.Y - i].BackColor == Color.Green || grid[start.X - XDistance][start.Y + i].BackColor == Color.Red) continue;
+
+
+                        grid[start.X - XDistance][start.Y - i].BackColor = Color.Blue;
+
+                    }
+                }
+            }
+            else if (start.X < end.X)
+            {
+                XDistance = end.X - start.X;
+                if (start.Y < end.Y)
+                {
+                    YDistance = end.Y - start.Y;
+                    for (int i = 0; i < XDistance; i++)
+                    {
+                        if (grid[start.X + i][start.Y].BackColor == Color.Green || grid[start.X + i][start.Y].BackColor == Color.Red) continue;
+
+
+
+                        grid[start.X + i][start.Y].BackColor = Color.Blue;
+
+
+
+                    }
+                    for (int i = 0; i < YDistance; i++)
+                    {
+                        if (grid[start.X + XDistance][start.Y + i].BackColor == Color.Green || grid[start.X + XDistance][start.Y + i].BackColor == Color.Red) continue;
+
+
+                        grid[start.X + XDistance][start.Y + i].BackColor = Color.Blue;
+
+                    }
+                }
+                else
+                {
+                    YDistance = start.Y - end.Y;
+                    for (int i = 1; i < XDistance + 1; i++)
+                    {
+                        if (grid[start.X + i][start.Y].BackColor == Color.Green || grid[start.X + i][start.Y].BackColor == Color.Red) continue;
+
+
+                        grid[start.X + i][start.Y].BackColor = Color.Blue;
+
+                    }
+                    for (int i = YDistance; i > 0; i--)
+                    {
+                        if (grid[start.X + XDistance][start.Y - i].BackColor == Color.Green || grid[start.X + XDistance][start.Y - i].BackColor == Color.Red) continue;
+                        grid[start.X + XDistance][start.Y - i].BackColor = Color.Blue;
+
+                    }
+                }
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    grid[i][j].BackColor = Color.Transparent;
+
+                }
+            }
+        }
+        public class Location
+        {
+            public Location(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public int X;
+            public int Y;
         }
     }
 }
